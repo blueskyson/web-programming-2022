23871c27f289453211c491dfb86df3c5e9e8a03c
blueskyson <blueskyson1401@gmail.com>
2022-03-09 00:19:26 +0800

    Add dragging file animation

--- hw1/script.js
+++ hw1/script.js
@@ -1,15 +1,50 @@
+// Check if browsers supports features we need.
+// This is an IIFE, see https://stackabuse.com/javascripts-immediately-invoked-function-expressions
+var isAdvancedUpload = function() {
+    var div = document.createElement('div');
+    return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) &&
+        'FormData' in window && 'FileReader' in window;
+}();
+
 $(document).ready(function() {
-    // fade in fade out
-    // https://www.w3schools.com/jquery/eff_fadeout.asp
+    // Enumeration of all landing page
+    const landing_page_01 = 1;
+    const landing_page_02 = 2;
+    const landing_page_03 = 3;
+
+    var current_page = landing_page_01;
     var fadeInTime = 500;
     var fadeOutTime = 500;
-    $("#upload_icon").click(function() {
-        /* hide */
-        $("#box").fadeOut(fadeOutTime);
-        $("#upload_icon").fadeOut(fadeOutTime);
-        $("#rotating_text").fadeOut(fadeOutTime);
-        /* show */
-        $("#box2").show();
-        $("#rotating_ray").fadeIn(fadeInTime);
-    });
+
+    // switch between landing page 01 and 02 
+    // https://www.w3schools.com/jquery/eff_fadeout.asp
+    function goto_landing_page_02() {
+        $(".landing_page_01").fadeOut(fadeOutTime);
+        $(".landing_page_02").fadeIn(fadeInTime);
+        current_page = landing_page_02;
+    }
+
+    function goto_landing_page_01() {
+        $(".landing_page_02").fadeOut(fadeOutTime);
+        $(".landing_page_01").fadeIn(fadeInTime);
+        current_page = landing_page_01;
+    }
+
+    // click upload_icon to go to landing page 02
+    $("#upload_icon").click(goto_landing_page_02);
+
+    // drag a file to go to landing page 02
+    // https://codepen.io/anastasialanz/pen/KdxxJx
+    if (isAdvancedUpload) {
+        $('.dropzone').on('dragenter', function() {
+            if (current_page == landing_page_01) {
+                goto_landing_page_02();
+            }
+        });
+        $('.dropzone_box2').on('dragleave', function() {
+            if (current_page == landing_page_02) {
+                goto_landing_page_01();
+            }
+        });
+    }
 });
