bec4faf958297f863d430961b3bdaaf63cef1b5f
blueskyson <blueskyson1401@gmail.com>
2022-03-08 20:46:16 +0800

    Turn CRLF to LF

--- hw1/script.js
+++ hw1/script.js
@@ -1,6 +1,6 @@
-// fade in fade out
-// https://www.w3schools.com/jquery/eff_fadeout.asp
 $(document).ready(function() {
+    // fade in fade out
+    // https://www.w3schools.com/jquery/eff_fadeout.asp
     var fadeInTime = 500;
     var fadeOutTime = 500;
     $("#upload_icon").click(function() {
