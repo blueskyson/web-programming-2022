191fe78930de6c90fe5ea8836d1eacc6db7510a5
blueskyson <blueskyson1401@gmail.com>
2022-03-09 04:02:59 +0800

    Almost complete landing page 03

--- hw1/script.js
+++ hw1/script.js
@@ -7,44 +7,61 @@ var isAdvancedUpload = function() {
 }();
 
 $(document).ready(function() {
-    // Enumeration of all landing page
-    const landing_page_01 = 1;
-    const landing_page_02 = 2;
-    const landing_page_03 = 3;
-
-    var current_page = landing_page_01;
     var fadeInTime = 500;
     var fadeOutTime = 500;
 
-    // switch between landing page 01 and 02 
+    // switch between landing_pages 
     // https://www.w3schools.com/jquery/eff_fadeout.asp
-    function goto_landing_page_02() {
-        $(".landing_page_01").fadeOut(fadeOutTime);
-        $(".landing_page_02").fadeIn(fadeInTime);
-        current_page = landing_page_02;
-    }
-
     function goto_landing_page_01() {
         $(".landing_page_02").fadeOut(fadeOutTime);
         $(".landing_page_01").fadeIn(fadeInTime);
-        current_page = landing_page_01;
     }
 
-    // click upload_icon to go to landing page 02
-    $("#upload_icon").click(goto_landing_page_02);
+    function goto_landing_page_02() {
+        $(".landing_page_01").fadeOut(fadeOutTime);
+        $(".landing_page_02").fadeIn(fadeInTime);
+    }
 
-    // drag a file to go to landing page 02
-    // https://codepen.io/anastasialanz/pen/KdxxJx
+    // FIXME: try to call functions one after another 
+    //        without using setTimeout()
+    function goto_landing_page_03() {
+        $(".landing_page_02").fadeOut(fadeInTime);
+        $(".landing_page_03 > .left_panel").fadeIn(fadeInTime);
+        setTimeout(function() {
+            $("#box3").animate({ 'left': "25%" }, fadeInTime);
+            $("#tissue").animate({ 'left': "25%" }, fadeInTime);
+            $(".landing_page_03 > .right_panel").fadeIn(fadeInTime);
+        }, fadeInTime);
+    }
+
+    // click upload_icon to go to landing_page_03
+
+    $("#upload_icon").click(function() {
+        setTimeout(goto_landing_page_02, 0);
+        setTimeout(goto_landing_page_03, fadeInTime);
+    });
+
     if (isAdvancedUpload) {
+        // drag a file to go to landing_page_02
+        // https://codepen.io/anastasialanz/pen/KdxxJx
         $('.dropzone').on('dragenter', function() {
             if (current_page == landing_page_01) {
                 goto_landing_page_02();
             }
         });
+
+        // go back to landing_page_01 if file leaves
         $('.dropzone_box2').on('dragleave', function() {
             if (current_page == landing_page_02) {
                 goto_landing_page_01();
             }
+        });
+
+        // go to landing_page_03 if file dropped
+        // https://codepen.io/hiralbest/pen/zKyVZW
+        $(".dropzone_box2").on('drop', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
         });
     }
 });
