0b0038864a0ec1d880e0b8859125bbcb823a3835
blueskyson <blueskyson1401@gmail.com>
2022-06-10 02:39:50 +0800

    Add http support code

--- hw2/ser.js
+++ hw2/ser.js
@@ -3,23 +3,42 @@
 import express from 'express'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser'
+import * as fs from 'fs'
+
+let https
+try {
+    https = await
+    import ('node:https')
+} catch (err) {
+    console.log('https support is disabled!')
+}
 
 const __filename = fileURLToPath(
     import.meta.url)
+
 const __dirname = dirname(__filename)
 
-const app = express()
-
 const port = 1401
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(`${__dirname}/static`))
+
+/* HTTPS Support */
+// const cert = fs.readFileSync("/home/tiffany/ssl/certificate.crt")
+// const ca = fs.readFileSync("/home/tiffany/ssl/ca_bundle.crt")
+// const key = fs.readFileSync("/home/tiffany/ssl/private.key")
+// https.createServer({
+//     key: key,
+//     cert: cert,
+//     ca: ca,
+// }, app).listen(port)
 
 app.listen(port, () => {
     console.log(`listening on port: ${port}`)
 })
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(`${__dirname}/static`))
 
 var students = {
     "E123456789": "Amy",
@@ -38,32 +57,32 @@ var students = {
     "Q785321256": "Nick",
     "E781344459": "Opera",
     "F100234798": "Penny",
-    "W744411551": "Question"
+    "W744411551": "Question",
 }
 
 
 app.get('/list', (req, res) => {
-    res.json(JSON.stringify(students));
+    res.json(JSON.stringify(students))
 })
 
 app.all('/search', (req, res) => {
-    var id = req.body.stuID;
+    var id = req.body.stuID
     if (id in students) {
-        res.send("Hello, " + students[id] + ".");
+        res.send("Hello, " + students[id] + ".")
     } else {
-        res.send("No such student.");
+        res.send("No such student.")
     }
 })
 
 app.all('/add', (req, res) => {
-    var id = req.body.stuID;
-    var name = req.body.stuName;
-    students[id] = name;
-    res.json(JSON.stringify(students));
+    var id = req.body.stuID
+    var name = req.body.stuName
+    students[id] = name
+    res.json(JSON.stringify(students))
 })
 
 app.all('/delete', (req, res) => {
-    var id = req.body.stuID;
-    delete students[id];
-    res.json(JSON.stringify(students));
+    var id = req.body.stuID
+    delete students[id]
+    res.json(JSON.stringify(students))
 })
